7dd4d6b0e31ec94ac124ba68a856d4e262a37e28
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-10-23 23:02:28 +0200

    something changed

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
 import "./Main.css";
 const Main = ({ data }) => {
+  const [check, setCheck] = useState(true);
+  console.log(check);
   return (
     <div className="main-container">
       <h1>Languages</h1>
       <div className="row">
-        {data.map((e, index) => {
-          const { name, img } = e;
-          return (
-            <div className="cards" key={index}>
-              <img src={img} alt="" />
-              <div className="cards-title">{name.toUpperCase()}</div>
-            </div>
-          );
-        })}
+        <div className="col" onClick={() => setCheck(!check)}>
+          {data.map((e, index) => {
+            return check ? (
+              <div className="cards" key={index}>
+                <img src={e.img} alt="" />
+                <div className="cards-title">{e.name.toUpperCase()}</div>
+              </div>
+            ) : (
+              <div className="cards" key={index}>
+                <ul>
+                  <li>{e.options[0]}</li>
+                  <li>{e.options[1]}</li>
+                  <li>{e.options[2]}</li>
+                </ul>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
