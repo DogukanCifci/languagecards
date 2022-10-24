7feb09934471add4427c098e28f95fac7993176e
DogukanCifci <107485142+DogukanCifci@users.noreply.github.com>
2022-10-24 17:02:15 +0200

    task completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Header from "./components/header/Header";
 import "./App.css";
-import Main from "./components/main/Main";
+
 import dataa from "./data";
+import Item from "./components/item/Item";
 function App() {
   console.log(dataa);
   return (
     <div>
       <Header />
-      <Main data={dataa} />
+      <Item data={dataa} />
     </div>
   );
 }
